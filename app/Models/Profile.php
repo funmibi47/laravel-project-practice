5ac39095e402f75f8c23b5d6d57274eb357d1546
funmibi47 <fh.iyanda@gmail.com>
2021-08-05 02:19:02 -0700

    first commit

---
+++ app/Models/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+     public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/9qRLiDTGD29krGVUun4AKqGiC6oKUu4h17MsUBwS.jpg';
+
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
